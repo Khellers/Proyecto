86a4d51b68e95ae4487a11c9e0e67a56a6a0cba7
Khellers <krhyslersjarquin1540@gmail.com>
2024-07-31 12:53:30 -0600

    cambio de proyecto vs

---
+++ XPPROJECT2/XPPROJECT2/XPPROJECT2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XPPROJECT2.Startup))]
+namespace XPPROJECT2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
