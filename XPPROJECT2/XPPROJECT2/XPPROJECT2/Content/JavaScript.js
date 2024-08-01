60dff5cc7649f7d3966e787a2b8bc114e21bd5fa
Juank37 <138148043+JC37D3Y@users.noreply.github.com>
2024-07-31 20:54:05 -0600

    cambios menores

---
+++ XPPROJECT2/XPPROJECT2/XPPROJECT2/Content/JavaScript.js
@@ -0,0 +1,16 @@
+﻿//$(document).ready(function () {
+//    $('#loader').hide();
+//});
+
+//$(window).on('load', function () {
+//    $('#loader').hide();
+//});
+
+//$(window).on('beforeunload', function () {
+//    $('#loader').show();
+//});
+//$(document).ready(function () {
+//    setTimeout(function () {
+//        $('#loader').hide();
+//    }, 2000);
+//});
