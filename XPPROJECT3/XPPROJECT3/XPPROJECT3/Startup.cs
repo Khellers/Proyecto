9e0f3ce47257207267883dc548202462843dec9d
Juank37 <138148043+JC37D3Y@users.noreply.github.com>
2024-08-03 14:42:37 -0600

    nuevo file

---
+++ XPPROJECT3/XPPROJECT3/XPPROJECT3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XPPROJECT3.Startup))]
+namespace XPPROJECT3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
